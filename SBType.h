28884b1305442ca4d6a4a367e966c593eb676647
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-11-09 00:28:44 +0200

    Update SBType.h

--- SBType.h
+++ SBType.h
@@ -1,78 +1,105 @@
-//
-//  SBType.h
-//  AssemblyProject1
-//
-//  Created by Mohamed El-Refai on 06/11/2024.
-//
-
-#ifndef SBType_h
-#define SBType_h
-
-//
-// SBTypeInstruction.h
-// AssemblyProject1
-//
+#ifndef SBTYPE_H
+#define SBTYPE_H
 
 #include "instruction.h"
+#include "registers.h"
 #include <iostream>
 
-using namespace std;
-
 class SBType : public instruction {
-
 private:
-    int rs1;      // Source register 1
-    int rs2;      // Source register 2
-    int imm4_1;   // Immediate bits 4-1
-    int imm10_5;  // Immediate bits 10-5
-    int imm11;    // Immediate bit 11
-    int imm12;    // Immediate bit 12 (most significant bit)
-    int func3;    // Function code 3
+    int rs1, rs2, imm4_1, imm10_5, imm11, imm12, func3;
 
 public:
     // Constructor to initialize all members, including inherited name and opCode
-    SBType(const string &name, int opCode, int rs1, int rs2, int imm4_1, int imm10_5, int imm11, int imm12, int func3)
-        : rs1(rs1), rs2(rs2), imm4_1(imm4_1), imm10_5(imm10_5), imm11(imm11), imm12(imm12), func3(func3) {
-            setName(name);     // Setting inherited member
-            setOpCode(opCode); // Setting inherited member
-    }
+    SBType(int opCode, int rs1, int rs2, int imm4_1, int imm10_5, int imm11, int imm12, int func3)
+        : instruction("SBType", opCode), rs1(rs1), rs2(rs2), imm4_1(imm4_1), imm10_5(imm10_5), imm11(imm11), imm12(imm12), func3(func3) {}
 
     // Method to assemble the immediate field
     int getImmediate() const {
         // Combine immediate bits according to RISC-V SB-type format
-        int imm = (imm12 << 12)     // imm[12]
-                | (imm11 << 11)     // imm[11]
-                | (imm10_5 << 5)    // imm[10:5]
-                | (imm4_1 << 1);    // imm[4:1]
+        int imm = (imm12 << 12) | (imm11 << 11) | (imm10_5 << 5) | (imm4_1 << 1);
         
-        // Sign extend the 13-bit immediate
-        if (imm & (1 << 12)) {  // If the 13th bit is set
-            imm |= 0xFFFFE000;  // Sign extend to 32 bits
+        // Sign-extend the 13-bit immediate
+        if (imm & (1 << 12)) {
+            imm |= 0xFFFFE000;  // Sign-extend to 32 bits if the sign bit is set
         }
-        
         return imm;
     }
+    
+    // Execution function specific to SB-type instructions
+    void execute(registers &registerFile, int &programCounter, int *memory) override {
+        int rs1Value = registerFile.get(rs1);  // Get value from rs1
+        int rs2Value = registerFile.get(rs2);  // Get value from rs2
+        int targetPC = programCounter + getImmediate();
 
-    // Getters for each register and function code
-    int getRS1() const {
-        return rs1;
-    }
+        switch (func3) {
+            case 0:  // BEQ: Branch if Equal
+                if (rs1Value == rs2Value) {
+                    programCounter = targetPC;
+                    cout << "Executing BEQ: Branching to " << programCounter << " as x" << rs1 << " == x" << rs2 << endl;
+                } else {
+                    cout << "Executing BEQ: No branch as x" << rs1 << " != x" << rs2 << endl;
+                }
+                break;
 
-    int getRS2() const {
-        return rs2;
-    }
+            case 1:  // BNE: Branch if Not Equal
+                if (rs1Value != rs2Value) {
+                    programCounter = targetPC;
+                    cout << "Executing BNE: Branching to " << programCounter << " as x" << rs1 << " != x" << rs2 << endl;
+                } else {
+                    cout << "Executing BNE: No branch as x" << rs1 << " == x" << rs2 << endl;
+                }
+                break;
 
-    int getFunc3() const {
-        return func3;
+            case 4:  // BLT: Branch if Less Than
+                if (rs1Value < rs2Value) {
+                    programCounter = targetPC;
+                    cout << "Executing BLT: Branching to " << programCounter << " as x" << rs1 << " < x" << rs2 << endl;
+                } else {
+                    cout << "Executing BLT: No branch as x" << rs1 << " >= x" << rs2 << endl;
+                }
+                break;
+
+            case 5:  // BGE: Branch if Greater or Equal
+                if (rs1Value >= rs2Value) {
+                    programCounter = targetPC;
+                    cout << "Executing BGE: Branching to " << programCounter << " as x" << rs1 << " >= x" << rs2 << endl;
+                } else {
+                    cout << "Executing BGE: No branch as x" << rs1 << " < x" << rs2 << endl;
+                }
+                break;
+
+            case 6:  // BLTU: Branch if Less Than Unsigned
+                if (static_cast<unsigned int>(rs1Value) < static_cast<unsigned int>(rs2Value)) {
+                    programCounter = targetPC;
+                    cout << "Executing BLTU: Branching to " << programCounter << " as x" << rs1 << " < x" << rs2 << " (unsigned)" << endl;
+                } else {
+                    cout << "Executing BLTU: No branch as x" << rs1 << " >= x" << rs2 << " (unsigned)" << endl;
+                }
+                break;
+
+            case 7:  // BGEU: Branch if Greater or Equal Unsigned
+                if (static_cast<unsigned int>(rs1Value) >= static_cast<unsigned int>(rs2Value)) {
+                    programCounter = targetPC;
+                    cout << "Executing BGEU: Branching to " << programCounter << " as x" << rs1 << " >= x" << rs2 << " (unsigned)" << endl;
+                } else {
+                    cout << "Executing BGEU: No branch as x" << rs1 << " < x" << rs2 << " (unsigned)" << endl;
+                }
+                break;
+
+            default:
+                cout << "Unknown SB-Type Instruction with func3 = " << func3 << endl;
+                break;
+        }
     }
 
     // Display function for debugging or logging
-    void display() const {
+    void display() const override {
         cout << "SB-Type Instruction - "
-        << "Name: " << getName()<< ", Opcode: " << getOpCode()
+             << "Name: " << getName() << ", Opcode: " << getOpCode()
              << ", RS1: " << rs1 << ", RS2: " << rs2
              << ", Immediate: " << getImmediate() << ", FUNC3: " << func3 << endl;
     }
 };
 
-#endif /* SBType_h */
+#endif /* SBTYPE_H */
